aa1d2fd5de155d05781a0c1c2514da33844776c8
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-09-04 10:08:40 -0300

    Adição de arquivos .js, para base de dados e funções

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "skatista brasileira que conquistou o mundo com seu talento e carisma. Conhecida como 'Fadinha', ela se tornou um ícone do esporte e uma inspiração para muitos jovens.",
+        link: "https://en.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "judoca brasileira, medalhista de ouro nos Jogos Olímpicos de Paris 2024. Conhecida por sua técnica e força, ela é um dos maiores nomes do judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "ginasta brasileira, medalhista olímpica e mundial. Conhecida por sua versatilidade e expressividade, ela é um dos maiores talentos da ginástica artística brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
