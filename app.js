a9ba1246c41f743a28c91f1bc13b60fbe269c9e6
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-09-06 10:27:33 -0300

    Alteração nas mensagens de pesquisa e projeto finalizado

--- app.js
+++ app.js
@@ -2,34 +2,49 @@ function pesquisar() {
   // Obtém a seção HTML onde os resultados serão exibidos
   let section = document.getElementById("resultados-pesquisa");
 
-  let campoPesquisa = document.getElementById("campo-pesquisa").value
-
-  let resultados = ""
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
   // se campoPesquisa for uma string sem nada
   if (!campoPesquisa) {
-      section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
+      section.innerHTML = "<p class='aviso-pesquisa'>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
       return 
-  }
+  };
+
+  // Transforma o conteúdo pesquisado em letras mínusculas
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = ""
 
   // Itera sobre cada dado da lista de dados
   for (let dado of dados) {
-      // se titulo includes campoPesquisa
-      if (dado.titulo.includes(campoPesquisa)) {
-          // cria um novo elemento
-          resultados += `
-          <div class="item-resultado">
-            <div> 
-              <h2>
-                <a href="#" target="_blank">${dado.titulo}</a>
-              </h2>
-              <p class="descricao-meta">${dado.descricao}</p>
-              <a href=${dado.link} target="_blank">Mais informações</a>
-            </div>
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+
+    // se titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // cria um novo elemento
+      resultados += `
+        <div class="item-resultado">
+          <div> 
+            <h2>
+              <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
           </div>
+        </div>
       `;
-      }
+    }
   }
+  //
+  if (!resultados) {
+    resultados = "<p class='aviso-pesquisa'> '" + campoPesquisa + "' não foi encontrado! </p>"
+  }
+
   // Atribui os resultados gerados à seção HTML
   section.innerHTML = resultados;
 }
--- dados.js
+++ dados.js
@@ -2,51 +2,61 @@ let dados = [
         {
             titulo: "Rayssa Leal",
             descricao: "skatista brasileira que conquistou o mundo com seu talento e carisma. Conhecida como 'Fadinha', ela se tornou um ícone do esporte e uma inspiração para muitos jovens.",
-            link: "https://en.wikipedia.org/wiki/Rayssa_Leal"
+            link: "https://en.wikipedia.org/wiki/Rayssa_Leal",
+            tags: "skate sk8 manobra"
         },
         {
             titulo: "Aline Silva",
             descricao: "lutadora de judô brasileira, medalhista olímpica em Londres 2012.",
-            link: "https://en.wikipedia.org/wiki.org/Aline_Silva_(judoka)"
+            link: "https://en.wikipedia.org/wiki.org/Aline_Silva_(judoka)",
+            tags: "lutadora judo judô "
         },
         {
             titulo: "Martine Grael",
             descricao: "velejadora brasileira, medalhista de ouro nos Jogos Olímpicos de Tóquio 2020.",
-            link: "https://en.wikipedia.org/wiki/Martine_Grael"
+            link: "https://en.wikipedia.org/wiki/Martine_Grael",
+            tags: "ouro velejadora"
         },
         {
             titulo: "Robert Scheidt",
             descricao: "velejador brasileiro, um dos maiores velejadores de todos os tempos, com diversas medalhas olímpicas.",
-            link: "https://en.wikipedia.org/wiki/Robert_Scheidt"
+            link: "https://en.wikipedia.org/wiki/Robert_Scheidt",
+            tags: "velejador "
         },
         {
             titulo: "Maurício Souza",
             descricao: "jogador de vôlei brasileiro, conhecido por sua força e bloqueios impressionantes.",
-            link: "https://en.wikipedia.org/wiki/Maur%C3%ADcio_Souza_(volleyball)"
+            link: "https://en.wikipedia.org/wiki/Maur%C3%ADcio_Souza_(volleyball)",
+            tags: "volei vôlei bloqueio força forca"
         },
         {
             titulo: "Ana Marcela Cunha",
             descricao: "nadadora de maratonas aquáticas, medalhista olímpica e mundial.",
-            link: "https://en.wikipedia.org/wiki/Ana_Marcela_Cunha"
+            link: "https://en.wikipedia.org/wiki/Ana_Marcela_Cunha",
+            tags: "nado nada nadadora maratona medelha"
         },
         {
             titulo: "Arthur Zanetti",
             descricao: "ginasta brasileiro, medalhista olímpico na barra fixa.",
-            link: "https://en.wikipedia.org/wiki/Arthur_Zanetti"
+            link: "https://en.wikipedia.org/wiki/Arthur_Zanetti",
+            tags: "ginastica medalha barra fixa"
         },
         {
             titulo: "Isadora Cerullo",
             descricao: "judoca brasileira, medalhista pan-americana e mundial.",
-            link: "https://en.wikipedia.org/wiki/Isadora_Cerullo"
+            link: "https://en.wikipedia.org/wiki/Isadora_Cerullo",
+            tags: "judo judô judoca medalha"
         },
         {
             titulo: "Leandro Barbosa",
             descricao: "jogador de basquete brasileiro, conhecido como 'Leandrinho', campeão da NBA.",
-            link: "https://en.wikipedia.org/wiki/Leandro_Barbosa"
+            link: "https://en.wikipedia.org/wiki/Leandro_Barbosa",
+            tags: "basquete leandrinho nba"
         },
         {
             titulo: "Marta Vieira da Silva",
             descricao: "futebolista brasileira, considerada uma das maiores jogadoras de futebol de todos os tempos.",
-            link: "https://en.wikipedia.org/wiki/Marta_Vieira_da_Silva"
+            link: "https://en.wikipedia.org/wiki/Marta_Vieira_da_Silva",
+            tags: "futebol melhor jogadora"
         }
 ];
